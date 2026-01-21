79a1a668f4c5dfe4bfc6b7ae83c4611622de54d1
毛遵杰 (Zaki) <1099298078@qq.com>
2026-01-21 17:20:56 +0800

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,6 @@
-import reflex as rx
-
-config = rx.Config(
-    app_name="be_holmes",
-)
+import reflex as rx  
+config = rx.Config(  
+    app_name="be_holmes",  
+    # 配置 Vite 允许的主机名  
+    frontend_packages=["reflex"],  
+)  
